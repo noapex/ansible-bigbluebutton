71796db32e5256682f4679a59ac6f5ab00014877
root <root@localhost.localdomain>
2016-07-27 01:57:05 -0300

    Deshabilito ipv6 para apt

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
 apt-get update
 apt-get install --assume-yes software-properties-common
 apt-add-repository --yes ppa:ansible/ansible
 apt-get update
 apt-get --assume-yes install ansible
+
